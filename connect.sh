d8f180e056f05180f0e9e7d753203ce2740fc97f
rmunoz <rmunoz@grupobme.es>
2020-03-23 17:42:01 +0100

    algo mas anonimizado y generico

--- connect.sh
+++ connect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DISPLAY=host.docker.internal:0
-openconnect 193.110.154.60 --no-cert-check -b  --csd-wrapper=/root/csd-wrapper.sh --authgroup=Teletrabajo_Emergencia -s "vpn-slice 10.0.0.0/8 bmeintranet.bme.com soporte.grupobme.es bmerrhh.bme.com hb1.bme.com inventario-unix.bme.com gitlab.grupobme.es bmelrcktb1.bme.com " 
+openconnect ${VPNSERVER} --no-cert-check -b  --csd-wrapper=/root/csd-wrapper.sh -u ${VPNUSER} --authgroup=${VPNGROUP} -s "vpn-slice $(cat /filtrosVPN | paste -sd " ") " 
 /bin/bash
